7d12b08f875c93dedfc0cd95c33e5c48672a83ff
truptimahajan01 <truptimahajan110@gmail.com>
2026-03-24 16:22:42 +0000

    Added fixtures and parametrized tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,2 +1,17 @@
-def test_data_read(test_data):
-    assert len(test_data) == 2
+import pytest
+
+@pytest.mark.parametrize("text,expected", [
+    ("hello", "HELLO"),
+    ("bye", "BYE")
+])
+
+def test_string(text,expected):
+    assert text.upper() == expected
+
+@pytest.mark.parametrize("a,b,expected", [
+    (2,2,4),
+    (3,3,6)
+])
+
+def test_addition(a,b,expected):
+    assert a + b == expected
