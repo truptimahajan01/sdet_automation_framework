e3022ec18c49cdf0461a3d217ba9fff4d2f8d27d
truptimahajan01 <truptimahajan110@gmail.com>
2026-04-18 18:32:36 +0000

    Added GitHub Actions CI workflow

--- api/base_api.py
+++ api/base_api.py
@@ -1,4 +1,6 @@
 import requests
+import time
+from utils.logger import get_logger
 
 class BaseAPIClient:
 
@@ -7,6 +9,8 @@ class BaseAPIClient:
         self.headers = {
             "Content-Type": "application/json"
         }
+        self.logger = get_logger()
+
 
     def get(self, endpoint):
         url = f"{self.base_url}{endpoint}"
@@ -26,4 +30,29 @@ class BaseAPIClient:
     def post(self, endpoint, data, headers=None):
         url = f"{self.base_url}{endpoint}"
         response = requests.post(url, json=data, headers=headers)
+        return response
+    
+    def get(self, endpoint, headers=None):
+        url = f"{self.base_url}{endpoint}"
+
+        start = time.time()
+
+        response = requests.get(url, headers=headers)
+
+        duration = round(time.time() - start, 3)
+
+        self.logger.info(
+            f"GET | {url} | {response.status_code} | {duration}"
+        )
+        return response
+    
+    def post(self, endpoint, data, headers=None):
+        url = f"{self.base_url}{endpoint}"
+
+        start = time.time()
+        response = requests.post(url, json=data, headers=headers)
+        duration = round(time.time() - start, 3)
+
+        self.logger.info(f"POST | {url} | {response.status_code} | {duration}s")
+
         return response
---
+++ utils/logger.py
@@ -0,0 +1,24 @@
+import logging
+
+def get_logger(name="framework_logger"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+
+        file_handler = logging.FileHandler(reports/framework.log")
+        file_handler.setLevel(logging.INFO)
+
+        formatter = logging.Formatter(
+            " %(asctime)s | %(levelname)s | %(name)s | %(message)s "
+        )
+
+        stream_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(stream_handler)
+        logger.addHandler(file_handler)
+
+    return logger
